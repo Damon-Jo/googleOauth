39c1a4109b09320665cc34101d20e02c738510b2
Damon-Jo <lunaticjky@gmail.com>
2024-01-17 22:16:27 -0800

    implemented server for oauth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import GoogleLogin from 'react-google-login';
 import {gapi} from 'gapi-script'
-import { useEffect } from 'react';
+import {useState, useEffect } from 'react';
 
 function App() {
   // const handleFailure = (result)=>{
   //   alert(JSON.stringify(result));
   // };
+
+  const [loginData, setLoginData] = useState(
+    localStorage.getItem('loginData')
+    ? JSON.parse(localStorage.getItem('loginData'))
+    : null
+  )
+
 
   const clientId=process.env.REACT_APP_GOOGLE_CLIENT_ID
   useEffect(()=>{
@@ -15,8 +22,26 @@ function App() {
     })
   },[])
 
-  const handleLogin = (googleData)=>{
-    console.log(googleData)
+  const handleLogin = async (googleData)=>{
+    // console.log(googleData)
+    const res = await fetch('/api/google-login', {
+      method : 'POST',
+      body:JSON.stringify({
+        token: googleData.tokenId,
+      }),
+      headers: {
+        'Content-Type' : 'application/json',
+      },
+    });
+
+    const data = await res.json();
+    setLoginData(data);
+    localStorage.setItem('loginData', JSON.stringify(data));
+  }
+
+  const handleLogout = () => {
+    localStorage.removeItem('loginData');
+    setLoginData(null);
   }
 
   return (
@@ -24,7 +49,14 @@ function App() {
       <header className="App-header">
         <h1>React Google Login App</h1>
         <div>
-          <GoogleLogin
+          {
+            loginData ? (
+              <div>
+                <h3>You logged in as {loginData.email}</h3>
+                <button onClick={handleLogout} >Logout</button>
+              </div>
+            ) : (
+              <GoogleLogin
               clientId={clientId}
               buttonText="Log in with Google"
               onSuccess={handleLogin}
@@ -32,6 +64,9 @@ function App() {
               cookiePolicy={'single_host_origin'}
           >
           </GoogleLogin>
+            )
+          }
+
         </div>
       </header>
     </div>
